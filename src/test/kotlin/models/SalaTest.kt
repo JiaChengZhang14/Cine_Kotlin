82240f5d2f67f69b7b5d95e5c53629cc8f9670f5
JiaCheng Zhang <jiachengmadrid@gmail.com>
2023-01-05 18:01:34 +0100

    cambios

--- src/test/kotlin/models/SalaTest.kt
+++ src/test/kotlin/models/SalaTest.kt
@@ -14,8 +14,8 @@ internal class SalaTest {
 
     @BeforeEach
     internal fun setUp() {
-        for(i in testMatrix.indices){
-            for(j in testMatrix[i].indices){
+        for (i in testMatrix.indices) {
+            for (j in testMatrix[i].indices) {
                 testMatrix[i][j] = FREE_SEAT
             }
         }
@@ -58,8 +58,8 @@ internal class SalaTest {
         testMatrix[1][0] = RESERVED_SEAT
 
         assertAll(
-            {assertEquals(true, Sala("").isSeatReserved("B:1", testMatrix))},
-            {assertEquals(false, Sala("").isSeatReserved("B:2", testMatrix))}
+            { assertEquals(true, Sala("").isSeatReserved("B:1", testMatrix)) },
+            { assertEquals(false, Sala("").isSeatReserved("B:2", testMatrix)) }
         )
     }
 
@@ -78,22 +78,22 @@ internal class SalaTest {
     @Test
     fun doesRowExistTest() {
         assertAll(
-            {assertEquals(true,Sala("").doesRowExist("A:1", row))},
-            {assertEquals(false,Sala("").doesRowExist("Z:26", row))},
-            {assertEquals(true,Sala("").doesRowExist("O:15", row))},
-            {assertEquals(false,Sala("").doesRowExist("V:16", row))},
-            {assertEquals(true,Sala("").doesRowExist("a:1", row))}
+            { assertEquals(true, Sala("").doesRowExist("A:1", row)) },
+            { assertEquals(false, Sala("").doesRowExist("Z:26", row)) },
+            { assertEquals(true, Sala("").doesRowExist("O:15", row)) },
+            { assertEquals(false, Sala("").doesRowExist("V:16", row)) },
+            { assertEquals(true, Sala("").doesRowExist("a:1", row)) }
         )
     }
 
     @Test
     fun doesColumnExistTest() {
         assertAll(
-            {assertEquals(true,Sala("").doesColumnExist("A:1", column))},
-            {assertEquals(false,Sala("").doesColumnExist("A:26", column))},
-            {assertEquals(true,Sala("").doesColumnExist("A:15", column))},
-            {assertEquals(false,Sala("").doesColumnExist("A:16", column))},
-            {assertEquals(true,Sala("").doesColumnExist("a:1", column))}
+            { assertEquals(true, Sala("").doesColumnExist("A:1", column)) },
+            { assertEquals(false, Sala("").doesColumnExist("A:26", column)) },
+            { assertEquals(true, Sala("").doesColumnExist("A:15", column)) },
+            { assertEquals(false, Sala("").doesColumnExist("A:16", column)) },
+            { assertEquals(true, Sala("").doesColumnExist("a:1", column)) }
         )
     }
 
@@ -128,32 +128,32 @@ internal class SalaTest {
         val entradaValida26 = "Z"
 
         assertAll(
-            { assertEquals(0,Sala("").rowLetterToNumber(entradaValida1))},
-            { assertEquals(1,Sala("").rowLetterToNumber(entradaValida2))},
-            { assertEquals(2,Sala("").rowLetterToNumber(entradaValida3))},
-            { assertEquals(3,Sala("").rowLetterToNumber(entradaValida4))},
-            { assertEquals(4,Sala("").rowLetterToNumber(entradaValida5))},
-            { assertEquals(5,Sala("").rowLetterToNumber(entradaValida6))},
-            { assertEquals(6,Sala("").rowLetterToNumber(entradaValida7))},
-            { assertEquals(7,Sala("").rowLetterToNumber(entradaValida8))},
-            { assertEquals(8,Sala("").rowLetterToNumber(entradaValida9))},
-            { assertEquals(9,Sala("").rowLetterToNumber(entradaValida10))},
-            { assertEquals(10,Sala("").rowLetterToNumber(entradaValida11))},
-            { assertEquals(11,Sala("").rowLetterToNumber(entradaValida12))},
-            { assertEquals(12,Sala("").rowLetterToNumber(entradaValida13))},
-            { assertEquals(13,Sala("").rowLetterToNumber(entradaValida14))},
-            { assertEquals(14,Sala("").rowLetterToNumber(entradaValida15))},
-            { assertEquals(15,Sala("").rowLetterToNumber(entradaValida16))},
-            { assertEquals(16,Sala("").rowLetterToNumber(entradaValida17))},
-            { assertEquals(17,Sala("").rowLetterToNumber(entradaValida18))},
-            { assertEquals(18,Sala("").rowLetterToNumber(entradaValida19))},
-            { assertEquals(19,Sala("").rowLetterToNumber(entradaValida20))},
-            { assertEquals(20,Sala("").rowLetterToNumber(entradaValida21))},
-            { assertEquals(21,Sala("").rowLetterToNumber(entradaValida22))},
-            { assertEquals(22,Sala("").rowLetterToNumber(entradaValida23))},
-            { assertEquals(23,Sala("").rowLetterToNumber(entradaValida24))},
-            { assertEquals(24,Sala("").rowLetterToNumber(entradaValida25))},
-            { assertEquals(25,Sala("").rowLetterToNumber(entradaValida26))},
+            { assertEquals(0, Sala("").rowLetterToNumber(entradaValida1)) },
+            { assertEquals(1, Sala("").rowLetterToNumber(entradaValida2)) },
+            { assertEquals(2, Sala("").rowLetterToNumber(entradaValida3)) },
+            { assertEquals(3, Sala("").rowLetterToNumber(entradaValida4)) },
+            { assertEquals(4, Sala("").rowLetterToNumber(entradaValida5)) },
+            { assertEquals(5, Sala("").rowLetterToNumber(entradaValida6)) },
+            { assertEquals(6, Sala("").rowLetterToNumber(entradaValida7)) },
+            { assertEquals(7, Sala("").rowLetterToNumber(entradaValida8)) },
+            { assertEquals(8, Sala("").rowLetterToNumber(entradaValida9)) },
+            { assertEquals(9, Sala("").rowLetterToNumber(entradaValida10)) },
+            { assertEquals(10, Sala("").rowLetterToNumber(entradaValida11)) },
+            { assertEquals(11, Sala("").rowLetterToNumber(entradaValida12)) },
+            { assertEquals(12, Sala("").rowLetterToNumber(entradaValida13)) },
+            { assertEquals(13, Sala("").rowLetterToNumber(entradaValida14)) },
+            { assertEquals(14, Sala("").rowLetterToNumber(entradaValida15)) },
+            { assertEquals(15, Sala("").rowLetterToNumber(entradaValida16)) },
+            { assertEquals(16, Sala("").rowLetterToNumber(entradaValida17)) },
+            { assertEquals(17, Sala("").rowLetterToNumber(entradaValida18)) },
+            { assertEquals(18, Sala("").rowLetterToNumber(entradaValida19)) },
+            { assertEquals(19, Sala("").rowLetterToNumber(entradaValida20)) },
+            { assertEquals(20, Sala("").rowLetterToNumber(entradaValida21)) },
+            { assertEquals(21, Sala("").rowLetterToNumber(entradaValida22)) },
+            { assertEquals(22, Sala("").rowLetterToNumber(entradaValida23)) },
+            { assertEquals(23, Sala("").rowLetterToNumber(entradaValida24)) },
+            { assertEquals(24, Sala("").rowLetterToNumber(entradaValida25)) },
+            { assertEquals(25, Sala("").rowLetterToNumber(entradaValida26)) }
 
 
         )
